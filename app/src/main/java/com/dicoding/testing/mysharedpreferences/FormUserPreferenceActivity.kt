50c1c1d65052df5d696477ebf8443f34802e8500
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2022-10-01 17:03:03 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/testing/mysharedpreferences/FormUserPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.testing.mysharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+    }
+}
